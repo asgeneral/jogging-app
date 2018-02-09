8ff8494f95b406f5839bb04a53c060e881f66323
asgeneral <asgeneral@yandex.ru>
2018-02-09 18:39:20 +0700

    Create header and registration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ***Material designers modules***//
+import { MatButtonModule, MatToolbarModule, MatInputModule } from '@angular/material';
 
+// ***Components***//
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
